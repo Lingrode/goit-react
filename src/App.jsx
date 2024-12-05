8efd59f0b601b9d666fe6205993d87c29078dd92
Illia Hrymalo <grimaloilya@gmail.com>
2024-12-04 22:33:59 -0700

    created FriendList

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,23 @@
 import userData from "./userData.json";
+import friends from "./friends.json";
 import Profile from "./components/Profile/Profile";
+import FriendList from "./components/FriendList/FriendList";
 
 function App() {
   const { username, tag, location, avatar, stats } = userData;
 
   return (
-    <Profile
-      name={username}
-      tag={tag}
-      location={location}
-      image={avatar}
-      stats={stats}
-    />
+    <>
+      <Profile
+        name={username}
+        tag={tag}
+        location={location}
+        image={avatar}
+        stats={stats}
+      />
+
+      <FriendList friends={friends} />
+    </>
   );
 }
 
